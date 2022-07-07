e01a854b8576d8da8d0d46cf62bf30dd9b39abb0
neuralskeptic <87091863+neuralskeptic@users.noreply.github.com>
2022-07-07 19:57:12 +0200

    refactoring (library/ -> src/, linting)

--- setup.py
+++ setup.py
@@ -2,5 +2,10 @@ from distutils.core import setup
 
 setup(
     name="smbrl",
-    packages=["library"],
+    version="0.1.1",
+    #    description='xxx',
+    #    license='MIT',
+    packages=["src", "scripts"],
+    zip_safe=False,
+    install_requires=[],
 )
--- src/utils/utils.py
+++ src/utils/utils.py
@@ -2,6 +2,8 @@ from collections import UserDict
 
 import numpy as np
 import torch
+from stable_baselines3.common.callbacks import BaseCallback
+from tqdm import tqdm
 
 EPS = torch.finfo(torch.float64).tiny
 
@@ -72,6 +74,48 @@ def to_torch(x):
 def autograd_tensor(x):
     """Same as torch.Tensor(x, requires_grad=True), but does not cause warnings."""
     return x.clone().detach().requires_grad_(True)
+
+
+class ProgressBarCallback(BaseCallback):
+    """
+    :param pbar: (tqdm.pbar) Progress bar object
+    """
+
+    def __init__(self, pbar):
+        super(ProgressBarCallback, self).__init__()
+        self._pbar = pbar
+        pbar.update(0)
+
+    def _on_step(self):
+        # breakpoint()
+        # Update the progress bar:
+        self._pbar.n = self.num_timesteps
+        # self._pbar.update(0)
+        # self._pbar.update(1)
+        self._pbar.refresh()
+        # breakpoint()
+        # breakpoint()
+        # if (self._pbar.n % 10 == 0):
+        # print(self._pbar.n)
+        # breakpoint()
+
+
+# this callback uses the 'with' block, allowing for correct initialisation and destruction
+class ProgressBarManager(object):
+    def __init__(self, total_timesteps):  # init object with total timesteps
+        self.pbar = None
+        self.total_timesteps = total_timesteps
+
+    def __enter__(
+        self,
+    ):  # create the progress bar and callback, return the callback
+        self.pbar = tqdm(total=self.total_timesteps, leave=True)
+        return ProgressBarCallback(self.pbar)
+
+    def __exit__(self, exc_type, exc_val, exc_tb):  # close the callback
+        self.pbar.n = self.total_timesteps
+        self.pbar.update(0)
+        self.pbar.close()
 
 
 class structdict(UserDict):
