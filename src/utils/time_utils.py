bbde6b687b87fafae4be1c3563df72668f8719c8
neuralskeptic <87091863+neuralskeptic@users.noreply.github.com>
2022-07-13 15:46:18 +0200

    replace dashes with underscores (shell: just one <c-w> to delete timestamp)

--- src/utils/time_utils.py
+++ src/utils/time_utils.py
@@ -5,4 +5,4 @@ from datetime import datetime
 def timestamp():
     "human readable, sortable, no spaces, LOCAL TIME"
     # return time.strftime("%Y-%m-%d_%H:%M:%S", time.localtime())
-    return datetime.now().strftime("%Y-%m-%d--%H-%M-%S")
+    return datetime.now().strftime("%Y_%m_%d__%H_%M_%S")
