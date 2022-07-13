53937edd1dac8dabdc677e60337d5c141c2df9b4
neuralskeptic <87091863+neuralskeptic@users.noreply.github.com>
2022-07-13 15:44:14 +0200

    start easily from terminal & args.yaml loading

--- scripts/run_policy.py
+++ scripts/run_policy.py
@@ -1,8 +1,11 @@
+import argparse
 import json
 import os
 
 import numpy as np
 import quanser_robots
+import yaml
+from experiment_launcher import run_experiment
 from mushroom_rl.core import Agent, Core
 from mushroom_rl.environments import Gym
 from mushroom_rl.utils.dataset import compute_J
@@ -11,27 +14,40 @@ from mushroom_rl.utils.preprocessors import StandardizationPreprocessor
 from src.utils.replay_agent import replay_agent
 from src.utils.seeds import fix_random_seed
 
-PATH_TO_EXPERIMENT = "./logs/tmp/SAC/Qube-100-v0/0/2022-07-11--18-41-16"
-AGENT_EPOCH = "2"
 
-args = json.load(open(os.path.join(PATH_TO_EXPERIMENT, "args.json")))
+def render_policy(
+    results_dir: str = "../logs/tmp/SAC/Qube-500-v0/10/2022-07-13--09-13-26",
+    agent_epoch: str = "end",
+    seed: int = -1,  ## IGNORED (only needed to run with with run_experiment)
+):
+    try:
+        with open(os.path.join(results_dir, "args.json")) as f:
+            args = json.load(f)
+    except:
+        with open(os.path.join(results_dir, "args.yaml")) as f:
+            args = yaml.load(f, Loader=yaml.Loader)
 
-# MDP
-mdp = Gym(args["env_id"], gamma=args["gamma"])
+    # MDP
+    mdp = Gym(args["env_id"], gamma=args["gamma"])
 
-# Fix seed
-fix_random_seed(args["seed"], mdp=None)
+    # Fix seed
+    fix_random_seed(args["seed"], mdp=None)
 
-# Agent
-agent_path = os.path.join(PATH_TO_EXPERIMENT, f"agent_{AGENT_EPOCH}.msh")
-agent = Agent.load(agent_path)
-prepro = None
-if args["preprocess_states"]:
-    prepro = StandardizationPreprocessor(mdp_info=mdp.info)
-core = Core(agent, mdp, preprocessors=[prepro] if prepro is not None else None)
+    # Agent
+    agent_path = os.path.join(results_dir, f"agent_{agent_epoch}.msh")
+    agent = Agent.load(agent_path)
+    prepro = None
+    if args["preprocess_states"]:
+        prepro = StandardizationPreprocessor(mdp_info=mdp.info)
+    core = Core(agent, mdp, preprocessors=[prepro] if prepro is not None else None)
 
-# Evaluate
-data = replay_agent(agent, core, 5, verbose=False, render=True)
-J = compute_J(data, args["gamma"])
-R = compute_J(data)
-print(f"J: {np.mean(J)}\nR: {np.mean(R)}")
+    # Evaluate
+    data = replay_agent(agent, core, 5, verbose=False, render=True)
+    J = compute_J(data, args["gamma"])
+    R = compute_J(data)
+    print(f"J: {np.mean(J)}\nR: {np.mean(R)}")
+
+
+if __name__ == "__main__":
+    # Leave unchanged
+    run_experiment(render_policy)
