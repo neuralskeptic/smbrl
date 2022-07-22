517c82bb494a49392f9b65d5088f7838d0b3d5ec
neuralskeptic <87091863+neuralskeptic@users.noreply.github.com>
2022-07-22 16:13:38 +0200

    comment unused dataset class

--- src/datasets/dataframe_datasets.py
+++ src/datasets/dataframe_datasets.py
@@ -4,22 +4,21 @@ from torch.utils.data import Dataset
 
 from src.utils.conversion_utils import df2torch
 
+# class DataFrameDataset(Dataset):
+#     """
+#     Dataset from pandas dataframe: pass dataframe and x and y labels.
+#     """
 
-class DataFrameDataset(Dataset):
-    """
-    Dataset from pandas dataframe: pass dataframe and x and y labels.
-    """
+#     def __init__(self, x_df: pd.DataFrame, y_df: pd.DataFrame, device: str = "cpu"):
+#         super(Dataset, self).__init__()
+#         self.x_data = df2torch(x_df).to(device)
+#         self.y_data = df2torch(y_df).to(device)
 
-    def __init__(self, x_df: pd.DataFrame, y_df: pd.DataFrame):
-        super(Dataset, self).__init__()
-        self.x_data = x_df
-        self.y_data = y_df
+#     def __len__(self):
+#         return self.x_data.shape[0]
 
-    def __len__(self):
-        return self.x_data.shape[0]
-
-    def __getitem__(self, idx):
-        return (
-            df2torch(self.x_data.iloc[idx, :]),
-            df2torch(self.y_data.iloc[idx, :]),
-        )
+#     def __getitem__(self, idx):
+#         return (
+#             self.x_data[idx, :],
+#             self.y_data[idx, :],
+#         )
