a7594b9a0190d00e6b974ec50a092edeabf27d43
neuralskeptic <87091863+neuralskeptic@users.noreply.github.com>
2022-11-02 20:52:32 +0100

    whitening wrapper for nn.Module

--- src/utils/whitening.py
+++ src/utils/whitening.py
@@ -1,4 +1,18 @@
 import torch
+import torch.nn as nn
+
+
+def whitening_metrics(X, Y):
+    # ZCA for X
+    x_mean = X.mean(axis=0)  # along N
+    x_centered = X - x_mean
+    sigma = x_centered.t() @ x_centered / X.shape[0]
+    U, S, _ = torch.svd(sigma)
+    w_ZCA = U @ torch.diag(torch.sqrt(S + 1e-6) ** -1) @ U.t()
+    # z-score for Y
+    y_mean = Y.mean(axis=0)  # along N
+    y_std = Y.std(axis=0)  # along N
+    return x_mean, w_ZCA, y_mean, y_std
 
 
 class Whitening(object):
@@ -22,9 +36,6 @@ class Whitening(object):
         U, S, _ = torch.svd(sigma)
         self.w_ZCA = U @ torch.diag(torch.sqrt(S + 1e-6) ** -1) @ U.t()
 
-        self.x_mean = self.x_mean
-        self.w_ZCA = self.w_ZCA
-
         # z-score for Y
         self.y_mean = Y.mean(axis=0)  # along N
         self.y_std = Y.std(axis=0)  # along N
@@ -46,3 +57,38 @@ class Whitening(object):
             return y
         else:
             return y * self.y_std.to(y.device) + self.y_mean.to(y.device)
+
+
+class WhiteningWrapper(nn.Module):
+    def __init__(self, nested: nn.Module):
+        super().__init__()
+        self.nested = nested  # implicitly in self._modules
+        kwargs = {"device": self.nested.device}
+        dim_x = list(self.nested.children())[0].in_features
+        dim_y = list(self.nested.children())[-1].out_features
+        self.register_buffer("x_mean", torch.zeros(dim_x, **kwargs))
+        self.register_buffer("w_ZCA", torch.eye(dim_x, **kwargs))
+        self.register_buffer("y_mean", torch.zeros(dim_y, **kwargs))
+        self.register_buffer("y_std", torch.ones(dim_y, **kwargs))
+
+    def __getattr__(self, name):  # called if attr not found here
+        if name == "nested":  # because pytorch wraps all self in a dict
+            return self._modules["nested"]
+        elif name in self._buffers:
+            return self._buffers[name]
+        else:
+            return getattr(self._modules["nested"], name)
+
+    def init_whitening(self, X, Y):
+        self.x_mean, self.w_ZCA, self.y_mean, self.y_std = whitening_metrics(X, Y)
+
+    def forward(self, x):
+        x_white = self.whitenX(x)
+        out_white = self.nested.forward(x_white)
+        return self.dewhitenY(out_white)
+
+    def whitenX(self, x):
+        return (x - self.x_mean) @ self.w_ZCA
+
+    def dewhitenY(self, y):
+        return y * self.y_std + self.y_mean
