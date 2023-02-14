464c7af9ec34913aabbad29130efc232d048773a
neuralskeptic <87091863+neuralskeptic@users.noreply.github.com>
2023-02-14 17:18:04 +0100

    by default do not whiten output (y)

--- src/utils/whitening.py
+++ src/utils/whitening.py
@@ -3,17 +3,21 @@ from functools import wraps
 import torch
 
 
-def whitening_metrics(X, Y):
+def whitening_metrics_x(X):
     # ZCA for X
     x_mean = X.mean(axis=0)  # along N
     x_centered = X - x_mean
     sigma = x_centered.t() @ x_centered / X.shape[0]
     U, S, _ = torch.svd(sigma)
     w_ZCA = U @ torch.diag(torch.sqrt(S + 1e-6) ** -1) @ U.t()
+    return x_mean, w_ZCA
+
+
+def whitening_metrics_y(Y):
     # z-score for Y
     y_mean = Y.mean(axis=0)  # along N
     y_std = Y.std(axis=0)  # along N
-    return x_mean, w_ZCA, y_mean, y_std
+    return y_mean, y_std
 
 
 def data_whitening(wrapped):
@@ -29,7 +33,7 @@ def data_whitening(wrapped):
             super().__init__(*args, **kwargs)
 
             self.register_buffer("do_whitening_x", torch.tensor(True))
-            self.register_buffer("do_whitening_y", torch.tensor(True))
+            self.register_buffer("do_whitening_y", torch.tensor(False))
             self.register_buffer("x_mean", torch.zeros(self.dim_x))
             self.register_buffer("w_ZCA", torch.eye(self.dim_x, self.dim_x))
             self.register_buffer("y_mean", torch.zeros(self.dim_y))
@@ -38,7 +42,10 @@ def data_whitening(wrapped):
         def init_whitening(self, X, Y, disable_x=False, disable_y=False):
             self.do_whitening_x = torch.tensor(not disable_x)
             self.do_whitening_y = torch.tensor(not disable_y)
-            self.x_mean, self.w_ZCA, self.y_mean, self.y_std = whitening_metrics(X, Y)
+            if self.do_whitening_x:
+                self.x_mean, self.w_ZCA = whitening_metrics_x(X)
+            if self.do_whitening_y:
+                self.y_mean, self.y_std = whitening_metrics_y(Y)
 
         def forward(self, x, *args, **kwargs):
             if self.do_whitening_x:
