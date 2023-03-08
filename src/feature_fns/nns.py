b56aebaf7369544d6687f3fa662f9fd59bcd4f0d
neuralskeptic <87091863+neuralskeptic@users.noreply.github.com>
2023-03-08 21:00:39 +0100

    add resnet + resnet skip connection analysis

--- src/feature_fns/nns.py
+++ src/feature_fns/nns.py
@@ -35,7 +35,7 @@ class TwoLayerNetwork(nn.Module):
         return self.act(self.l2(self.act(self.l1(x))))
 
 
-class NeuralNetwork(nn.Module):
+class MultiLayerPerceptron(nn.Module):
     def __init__(self, d_in, n_hidden_layers, d_hidden, d_out, activation="leakyrelu"):
         assert activation in ACTIVATIONS
         super().__init__()
@@ -49,10 +49,21 @@ class NeuralNetwork(nn.Module):
         for l in self.layers[:-1]:
             x = self.act(l(x))
         return self.layers[-1](x)  # no output activation!
+
+
+class ResidualNetwork(MultiLayerPerceptron):
+    def forward(self, x):
+        lx1 = self.layers[0](x)
+        x = lx1 + self.act(lx1)  # d_in != d_hidden (map, then skip)
+        for l in self.layers[1:-1]:  # d_hidden == d_hidden (skip directly)
+            lx = l(x)
+            x = x + self.act(lx)
+        lxn = self.layers[-1](x)
+        x = lxn + self.act(lxn)  # d_hidden != d_out (map, then skip)
         return x
 
 
-class NormalizedResidualNetwork(nn.Module):
+class SpectralNormResidualNetwork(nn.Module):
     def __init__(self, d_in, n_hidden_layers, d_hidden, d_out, activation="leakyrelu"):
         assert activation in ACTIVATIONS
         super().__init__()
@@ -64,7 +75,9 @@ class NormalizedResidualNetwork(nn.Module):
         self.register_buffer("W", torch.randn(d_hidden, d_out // 2))
 
     def forward(self, x):
-        for l in self.layers:
+        lx1 = self.layers[0](x)
+        x = lx1 + self.act(lx1)  # d_in != d_hidden (map, then skip)
+        for l in self.layers[1:]:  # d_hidden == d_hidden (skip directly)
             lx = l(x)
-            x = lx + self.act(lx)
+            x = x + self.act(lx)
         return torch.cat([torch.cos(x @ self.W), torch.sin(x @ self.W)], -1)
